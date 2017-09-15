7dc2918557dd6b1c5c6d4e715c23e0ef7a845ecb
james <james@africaloop.com>
2017-09-15 17:54:24 +0000

    still working

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.login_header')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -66,4 +66,64 @@
         </div>
     </div>
 </div>
+ -->
+
+ <div class="app-content content container-fluid">
+      <div class="content-wrapper">
+        <div class="content-header row">
+        </div>
+        <div class="content-body"><section class="flexbox-container">
+    <div class="col-md-4 offset-md-4 col-xs-10 offset-xs-1  box-shadow-2 p-0">
+        <div class="card border-grey border-lighten-3 m-0">
+            <div class="card-header no-border">
+                <div class="card-title text-xs-center">
+                    <div class="p-1"><img src="{{asset('app-assets/images/logo/robust-logo-dark.png')}}" alt="branding logo"></div>
+                </div>
+                <h6 class="card-subtitle line-on-side text-muted text-xs-center font-small-3 pt-2"><span>Login with Robust</span></h6>
+            </div>
+            <div class="card-body collapse in">
+                <div class="card-block">
+                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <fieldset class="form-group position-relative has-icon-left mb-0{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <input type="email" class="form-control form-control-lg input-lg" id="email" name="email" placeholder="Enter E-Mail" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="form-control-position">
+                                <i class="icon-head"></i>
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-group position-relative has-icon-left{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <input type="password" class="form-control form-control-lg input-lg" id="password" name="password" placeholder="Enter Password" required>
+                            <div class="form-control-position">
+                                <i class="icon-key3"></i>
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-group row">
+                            <div class="col-md-6 col-xs-12 text-xs-center text-md-left">
+                                <fieldset>
+                                    <input type="checkbox" id="remember-me" name="remember" class="chk-remember">
+                                    <label for="remember-me"> Remember Me</label>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-6 col-xs-12 text-xs-center text-md-right"><a href="recover_password.php" class="card-link">Forgot Password?</a></div>
+                        </fieldset>
+                        <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block"><i class="icon-unlock2"></i> Login</button>
+                    </form>
+                </div>
+            </div>
+            <div class="card-footer">
+                <div class="">
+                    <p class="float-sm-left text-xs-center m-0"><a href="recover_password.php" class="card-link">Recover password</a></p>
+                    <p class="float-sm-right text-xs-center m-0">New to Robust? <a href="register.php" class="card-link">Sign Up</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+        </div>
+      </div>
+    </div>
+
+
 @endsection
