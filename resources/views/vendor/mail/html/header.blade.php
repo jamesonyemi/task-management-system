c143fe6729a2c2b04a4bfb6c8dee3f84b02c9153
james <james@africaloop.com>
2017-10-18 16:14:30 +0000

    email notification for forget or recover password is working

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,6 @@
 <tr>
     <td class="header">
+    	<div class="nav-item" style="background-color: #1d2b36"><a href="#" class="navbar-brand nav-link"><img alt="branding logo" src="{{asset('app-assets/images/logo/robust-logo-light.png')}}" data-expand="{{asset('app-assets/images/logo/robust-logo-light.png')}}" data-collapse="{{asset('app-assets/images/logo/robust-logo-small.png')}}" class="brand-logo"></a></div>
         <a href="{{ $url }}">
             {{ $slot }}
         </a>
