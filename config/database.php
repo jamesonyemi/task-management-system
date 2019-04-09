b4a64def51e0757ee25d7be906fddab404235a19
JAMESADELAJA\james <jamesugbosanmi@yahoo.com>
2019-04-09 23:20:45 +0000

    added database.php and composer.json

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'remotemysql.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'tms'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'RuPZ2hleSb'),
+            'username' => env('DB_USERNAME', 'RuPZ2hleSb'),
+            'password' => env('DB_PASSWORD', 'njqGXkWlET'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
